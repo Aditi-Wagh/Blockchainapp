a5f565613aa9b881ea81ebf5d20e38963fb2e34c
Aditi-Wagh <106764459+Aditi-Wagh@users.noreply.github.com>
2024-09-22 14:36:00 +0530

    Updates

---
+++ precache-manifest.c797c33d85f61cd1705f4dd8e09dca08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "adc9194edbdc8b9707ea",
+    "url": "/Blockchainapp/static/js/main.adc9194e.chunk.js"
+  },
+  {
+    "revision": "29fbf0165d57bb812086",
+    "url": "/Blockchainapp/static/css/1.65654658.chunk.css"
+  },
+  {
+    "revision": "29fbf0165d57bb812086",
+    "url": "/Blockchainapp/static/js/1.29fbf016.chunk.js"
+  },
+  {
+    "revision": "0236a53e54594ea434cb",
+    "url": "/Blockchainapp/static/js/runtime~main.0236a53e.js"
+  },
+  {
+    "revision": "d348a6ccceeffd9463551d2d17f66f95",
+    "url": "/Blockchainapp/index.html"
+  }
+];
